f28ce783a24d8b9596ffdcb97477db963afda634
liuhanqu <120936337@qq.com>
2018-05-28 14:45:05 +0800

    reverse

---
+++ algorithms/easy/reverse/reverse.js
@@ -0,0 +1,18 @@
+// description:  https://leetcode-cn.com/problems/reverse-integer/description/
+// 反转整数
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  var ret = 0;
+  while (x) {
+    ret = ret * 10 + x % 10;
+    x = parseInt(x / 10);
+  }
+  if (ret > Math.pow(2, 31) - 1 || ret < Math.pow(-2, 31)) {
+    ret = 0;
+  }
+  return ret;
+};
